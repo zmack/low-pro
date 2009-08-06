55f0ba4187b5960efb03291e3074ee80d827916a
Andrei Bocan <zmaxor@gmail.com>
2009-08-06 15:22:13 +0300

    Re-add confirm. I was actually using that :<

--- src/core_behaviors.js
+++ src/core_behaviors.js
@@ -14,6 +14,10 @@ Remote.Base = {
     this._bindCallbacks();
   },
   _makeRequest : function(options) {
+    if (options.confirm && !confirm(options.confirm)) {
+      return false;
+    }
+
     if (options.update) new Ajax.Updater(options.update, options.url, options);
     else new Ajax.Request(options.url, options);
     return false;
